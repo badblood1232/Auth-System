0d40ce95a471f3ba75e899a173e31ee0be3d10a9
badblood1232 <ngujojohn123@gmail.com>
2025-10-21 01:09:33 +0900

    added logic for creating database creation

---
+++ server/database/schema.sql
@@ -0,0 +1,8 @@
+
+
+-- Then your table creation statements
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(255)
+);
--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,38 @@
 const mysql = require('mysql2')
 const dotenv = require('dotenv')
 dotenv.config()
-
+const fs = require('fs')
+const path = require('path')
 
 const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  
+})
+
+const sqlpath = path.join(__dirname, '../database/schema.sql')
+const sql = fs.readFileSync(sqlpath).toString()
+
+
+db.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_database}`,(err,result)=>{
+    if (err) throw err
+    console.log("table created")
+})
+
+
+
+const dbWithDatabase = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_database,
 })
 
-module.exports = db
+dbWithDatabase.query(sql, (err, result) =>{
+    if(err) throw err
+    console.log('Database and table created!')
+    dbWithDatabase.end()
+})
+
+module.exports = dbWithDatabase
